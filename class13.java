dbefd2b1475951d8fa90695224b2673ef80e7f05
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-08 22:35:15 +0800

    Add files via upload

--- class13.java
+++ class13.java
@@ -1,20 +1,32 @@
-/**
- * 
- */
 package java08;
 
-/**
- * @author Acer
- *
- */
+class CCircle {
+	double area;
+	double area(double r) {
+		area = r * r * 3.14;
+		return (double) area;
+	}
+	float area(float r) {
+		area = r * r * 3.14;
+		return (float) area;
+	}
+	int area(int r) {
+		area = r * r * 3.14;
+		return (int) area;
+	}
+	public void show() {
+		System.out.println("area=" + area);
+	}
+}
 public class class13 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CCircle a = new CCircle();
+		a.area(2);
+		a.show();
+		a.area(2.2f);
+		a.show();
+		a.area(2.2);
+		a.show();
 	}
-
 }
