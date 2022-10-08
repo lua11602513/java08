81844a3097dca44af56df7a8f92f87d98cabc3c3
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-10-08 22:34:25 +0800

    Add files via upload

---
+++ class13.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package java08;
+
+/**
+ * @author Acer
+ *
+ */
+public class class13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
